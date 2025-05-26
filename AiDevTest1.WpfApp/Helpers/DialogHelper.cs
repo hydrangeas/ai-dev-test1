6573e536f0913f6ebf030a09c0063c04a6363654
Akira Otaka <hydrangeas.project@gmail.com>
2025-05-26 23:44:41 +0900

    feat: DialogHelper静的クラスによるダイアログ機能実装
    - issue #13の要件を満たす静的ヘルパークラスパターンで実装
    - シンプルで再利用可能なダイアログ表示機能を提供
    - WPF MessageBoxを使用した成功/失敗ダイアログ表示
    
    Closes #13

---
+++ AiDevTest1.WpfApp/Helpers/DialogHelper.cs
@@ -0,0 +1,28 @@
+using System.Windows;
+
+namespace AiDevTest1.WpfApp.Helpers
+{
+  /// <summary>
+  /// ダイアログ表示のためのヘルパークラス
+  /// </summary>
+  public static class DialogHelper
+  {
+    /// <summary>
+    /// 成功ダイアログを表示します
+    /// </summary>
+    /// <param name="message">表示するメッセージ</param>
+    public static void ShowSuccess(string message)
+    {
+      MessageBox.Show(message, "成功", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+
+    /// <summary>
+    /// 失敗ダイアログを表示します
+    /// </summary>
+    /// <param name="message">表示するメッセージ</param>
+    public static void ShowFailure(string message)
+    {
+      MessageBox.Show(message, "失敗", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+  }
+}
